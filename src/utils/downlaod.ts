7f1245be8692f847476501c9a9bf141a192413dd
Utkarsh Tiwari <utkarshweb2023@gmail.com>
2025-11-17 12:27:39 +0530

    Implement torrent message parsing and handling for peer communication.

--- src/utils/downlaod.ts
+++ src/utils/downlaod.ts
@@ -1,6 +1,7 @@
 import { Socket } from "node:net";
 import type { Peer, Torrent } from "../types";
 import { getPeers } from "./tracker";
+import { buildInterested, buildRequest } from "./messages";
 
 export function torrent(torrents: Torrent) {
   getPeers(torrents, (peers) => {
@@ -38,4 +39,66 @@ function onWholeMsg(socket: Socket, callback: (data: Buffer) => void) {
       handshake = false;
     }
   });
+}
+
+// 1
+function msgHandler(msg, socket, pieces, queue) {
+  if (isHandshake(msg)) {
+    socket.write(buildInterested());
+  } else {
+    const m = parse(msg);
+
+    if (m.id === 0) chokeHandler(socket);
+    // 1
+    if (m.id === 1) unchokeHandler(socket, pieces, queue);
+    if (m.id === 4) haveHandler(m.payload);
+    if (m.id === 5) bitfieldHandler(m.payload);
+    if (m.id === 7) pieceHandler(m.payload);
+  }
+}
+
+function isHandshake(msg) {
+  return (
+    msg.length === msg.readUInt8(0) + 49 &&
+    msg.toString("utf8", 1) === "BitTorrent protocol"
+  );
+}
+
+function chokeHandler(socket) {
+  socket.end();
+}
+
+// 1
+function unchokeHandler(socket, pieces, queue) {
+  queue.choked = false;
+  // 2
+  requestPiece(socket, pieces, queue);
+}
+
+function haveHandler() {
+  // ...
+}
+
+function bitfieldHandler() {
+  // ...
+}
+
+function pieceHandler() {
+  // ...
+}
+
+//1
+function requestPiece(socket, pieces, queue) {
+  //2
+  if (queue.choked) return null;
+
+  while (queue.queue.length) {
+    const pieceIndex = queue.shift();
+    if (pieces.needed(pieceIndex)) {
+      // need to fix this
+      socket.write(buildRequest(pieceIndex));
+      pieces.addRequested(pieceIndex);
+      break;
+    }
+  }
 }
